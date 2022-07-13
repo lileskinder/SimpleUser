aa161d754f8123c634237f6bb2b0087e6f4d9e6d
lileskinder <33198174+lileskinder@users.noreply.github.com>
2022-07-13 19:11:53 -0400

    initail commit

---
+++ src/main/java/com/example/simpleuser/SimpleUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleUserApplication.class, args);
+    }
+
+}
